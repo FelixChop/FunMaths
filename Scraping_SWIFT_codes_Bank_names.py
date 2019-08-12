d42735133830f768f3131f49d6052d5b4ae26cdc
Fabien Lebas <fabien.lebas@decathlon.com>
2019-08-12 23:35:50 +0200

    resolves the issue in the requests due to missing header

--- Scraping_SWIFT_codes_Bank_names.py
+++ Scraping_SWIFT_codes_Bank_names.py
@@ -13,11 +13,13 @@ def next_page(soup):
 
 res = pd.DataFrame()
 url = "http://bank-code.net/country/FRANCE-%28FR%29/"
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'
+headers = { 'User-Agent' : user_agent }
 counter = 0
 
 while True:
 	print(counter)
-	page = requests.get(url)
+	page = requests.get(url, headers=headers)
 	soup = bs4.BeautifulSoup(page.content, 'lxml')
 	table = soup.find(name='table', attrs={'id':'tableID'})
 	res = res.append(table_to_df(table))
